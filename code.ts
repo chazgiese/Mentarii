e7c5aaad800636f08d75db098ec35af2e844daac
Chaz Giese <chazgiese@gmail.com>
2025-07-17 19:14:41 -0400

    Remove unused ui.html and update comments

--- code.ts
+++ code.ts
@@ -1,14 +1,15 @@
 // This file holds the main code for plugins. Code in this file has access to
 // the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
+// You can access browser APIs in the bundled HTML page (dist/index.html after
+// build) which runs with a full browser environment. See
+// https://www.figma.com/plugin-docs/how-plugins-run for more details.
 
 // Runs this code if the plugin is run in Figma
 if (figma.editorType === 'figma') {
   // This plugin will open a window to prompt the user to enter a number, and
   // it will then create that many rectangles on the screen.
 
-  // This shows the HTML page in "ui.html".
+  // Display the HTML page defined in `manifest.json` (dist/index.html).
   figma.showUI(__html__);
 
   // Calls to "parent.postMessage" from within the HTML page will trigger this
